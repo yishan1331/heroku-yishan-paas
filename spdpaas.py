6d04aacacff5c6b67cc86a6c8fa3e0254a953100
Yishan <yishanjob13@gmail.com>
2021-05-30 19:06:11 +0800

    import apiPortal.py

--- spdpaas.py
+++ spdpaas.py
@@ -1,9 +1,8 @@
 #assign app from sessionMgr
 print "-----into spdpaas.py-----"
 from app import *
-print "-----spdpaas import app over-----"
-# appSapido = appPaaS
-# import app.apiPortal
+appSapido = appPaaS
+import app.apiPortal
 
 # if __name__ == "__main__":
 #     print "-----spdpaas.py __main__-----"

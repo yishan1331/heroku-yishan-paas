632ea578c85ac64fcfb513176ba3ac0e0b1ebc65
Yishan <yishanjob13@gmail.com>
2021-05-30 16:09:01 +0800

    debug error config

--- spdpaas.py
+++ spdpaas.py
@@ -1,7 +1,7 @@
 #assign app from sessionMgr
 from app import *
 appSapido = appPaaS
-import app.apiPortal
+# import app.apiPortal
 
 if __name__ == "__main__":
     print "-----spdpaas.py __main__-----"

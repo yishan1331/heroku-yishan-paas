92f5a650e86948a9fcee95dc946f1047d4282907
Yishan <yishanjob13@gmail.com>
2021-06-14 22:39:48 +0800

    update templates

--- templates/static/php/globalvar.php
+++
@@ -1,9 +0,0 @@
-<?php
-//https://blog.csdn.net/leedaning/article/details/72781734
-$HTTPHOST = explode(":",$_SERVER['HTTP_HOST']);
-$publicIP = $HTTPHOST[0];
-
-if (!filter_var($HTTPHOST[0], FILTER_VALIDATE_IP) or $_SERVER['SERVER_PORT'] != "3687") {
-    $publicIP = "211.75.243.185";
-}
-$publicPort = "3687";

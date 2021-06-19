9ea7f7f2e6b68d67253efd6d9539585b509ef2ce
Yishan <yishanjob13@gmail.com>
2021-06-20 03:21:41 +0800

    add socket run

--- spdpaas.py
+++ spdpaas.py
@@ -3,6 +3,8 @@ print "-----into spdpaas.py-----"
 from app import *
 import app.apiPortal
 
+socketio.run(appPaaS,debug=True)
+
 # if __name__ == "__main__":
 #     print "-----spdpaas.py __main__-----"
 #     appSapido.run()
